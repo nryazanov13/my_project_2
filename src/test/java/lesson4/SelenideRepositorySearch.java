a0e7bf51c62e023148477580a6bebc04b043453c
Nikita Ryazanov <riazanovnikita@gmail.com>
2025-08-15 16:04:48 +0300

    added edited build gradle and added SelenideRepositorySearch class

---
+++ src/test/java/lesson4/SelenideRepositorySearch.java
@@ -0,0 +1,21 @@
+package lesson4;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class SelenideRepositorySearch {
+
+    @BeforeAll
+    static void setUp(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        //Configuration.holdBrowserOpen = true;
+    }
+
+
+    void findSelenideOnGitHub(){
+
+    }
+
+}
