151d4490e8dc0a311ef3d7e4422a82e4e91c5e78
Nikita Ryazanov <riazanovnikita@gmail.com>
2025-08-18 16:00:25 +0300

    drag and drop class was finished

--- src/test/java/lesson5/DragAndDrop.java
+++ src/test/java/lesson5/DragAndDrop.java
@@ -5,27 +5,37 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
     @BeforeAll
     static void setUp(){
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://github.com";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
     }
 
 
     @Test
     void shouldDragAndDropWithSelenideActionsTest() {
-        //Open selenide page on GitHub
-        open("/selenide/selenide");
-        //Click on wiki-tab
-        $("#wiki-tab").click();
-        //Check if there is "soft assertions" text on the page
-        $("#wiki-body").$("ul").shouldHave(text("Soft assertions"));
-        //Open "soft assertions" page
-        $("#wiki-body").$$("a").findBy(text("Soft assertions")).click();
+        //Open drag and drop page
+        open("/drag_and_drop");
+        //Move and drop element
+        //Calculate coordinates
+        int xOffset = $("#column-b").getLocation().getX() - $("#column-a").getLocation().getX();
+        int yOffset = $("#column-b").getLocation().getY() - $("#column-a").getLocation().getY();
+
+        // Perform drag and drop
+        actions()
+                .moveToElement($("#column-a"))
+                .clickAndHold()
+                .moveByOffset(xOffset, yOffset)
+                .release()
+                .perform();
+
+        // Check if column was changed
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b header").shouldHave(text("A"));
     }
 }
--- src/test/java/lesson5/GitHubEnterpriseHover.java
+++ src/test/java/lesson5/GitHubEnterpriseHover.java
@@ -5,27 +5,29 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class GitHubEnterpriseHover {
     @BeforeAll
     static void setUp(){
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
     }
 
 
     @Test
     void shouldFindEnterpriseByHoverTest() {
+
         //Open selenide page on GitHub
-        open("/selenide/selenide");
-        //Click on wiki-tab
-        $("#wiki-tab").click();
-        //Check if there is "soft assertions" text on the page
-        $("#wiki-body").$("ul").shouldHave(text("Soft assertions"));
-        //Open "soft assertions" page
-        $("#wiki-body").$$("a").findBy(text("Soft assertions")).click();
+        open("https://github.com");
+        //Hover mouse on "Solutions"
+        $$("li").findBy(text("Solutions")).hover();
+        //Click on "Enterprise" text
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+        //Create variable for enterprise text
+        String enterpriseText = "The AI-powered developer platform";
+        //Check if text == enterpriseText variable
+        $("#hero-section-brand-heading").shouldHave(text(enterpriseText));
+
     }
 }
